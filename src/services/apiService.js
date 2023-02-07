f0f1a7fc7ab92d8ff73a9611c91a7042cbab9f5c
unknown <datnguyen5121@gmail.com>
2023-02-07 10:19:12 +0700

    helo

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -49,5 +49,8 @@ const getQuizByUser = () => {
 const getDataQuiz = (id) => {
     return axios.get(`/api/v1/questions-by-quiz?quizId=${id}`)
 }
+const postSubmitQuiz = (data) => {
+    return axios.post(`/api/v1/quiz-submit`,{...data})
+}
 export {postCreateNewUser, getAllUsers, putUpdateUser, deleteUser, 
-    getUserWithPaginate, postLogin,handleRegister,getQuizByUser,getDataQuiz}
+    getUserWithPaginate, postLogin,handleRegister,getQuizByUser,getDataQuiz,postSubmitQuiz}
--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -12,7 +12,7 @@ const instance = axios.create({
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
   const access_token = store?.getState()?.user?.account?.access_token;
-  console.log(access_token);
+  // console.log(access_token);
   config.headers["Authorization"] = `Bearer ${access_token}`;
     // Do something before request is sent
     NProgress.start();
